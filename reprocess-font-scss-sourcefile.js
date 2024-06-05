18eec18f65b098d4a37ea01ff52ad4444f0b0947
Ger Hobbelt <ger@hobbelt.com>
2024-06-05 20:10:09 +0200

    added node script to check and update/tweak the SCSS source files to match the present state of affairs re fonts availability.

---
+++ reprocess-font-scss-sourcefile.js
@@ -0,0 +1,100 @@
+
+const path = require("path");
+const fs = require("fs");
+const g = require('glob');
+
+//console.log({ argv: process.argv });
+
+let scss_filepath = process.argv[2];
+
+console.log("Processing SCSS file:", scss_filepath);
+
+let src = fs.readFileSync(scss_filepath, "utf8");
+let edited = false;
+
+//   src: url('Cinzel/fonts/ttf/Cinzel-Regular.ttf') format('truetype');
+//   src: url('Commissioner/fonts/webfonts/woff2/CommissionerFlair-Thin.woff2') format('woff2');
+
+function cleanup4glob(p) {
+	return p
+	.replace(/[\[\],]/g, '?');
+}
+
+src = src
+.replace(/src: url\(([^)]+)\)([^;]+);/g, function (m, p1, p2) {
+	let fp = p1.trim();
+	if (/['"]/.test(fp[0]) && (fp[0] === fp[fp.length - 1])) {
+		fp = fp.substring(1, fp.length - 1);
+	}
+	//console.log({p1, p2, m, fp});
+	
+	if (!fs.existsSync(fp)) {
+		console.log("Font file is missing:", fp);
+		let rootdir = fp.replace(/\/.*$/, "");
+		let fname = path.basename(fp);
+		//console.log({rootdir, fname});
+		
+		let is_variable = /-variations/.test(p2);
+		let variable_suffix = (is_variable ? '-variations' : '');
+
+		const glob_cfg = {
+		  // only want the files, not the dirs
+          nodir: true,
+		  ignore: {
+			ignored: p => p.isNamed('glyphs') || p.isNamed('fonts-tests') || p.isNamed('older sources') || p.isNamed('legacy') || p.isNamed('old') || p.isNamed('_temp'),
+			childrenIgnored: p => p.isNamed('glyphs') || p.isNamed('fonts-tests') || p.isNamed('older sources') || p.isNamed('legacy') || p.isNamed('old') || p.isNamed('_temp'),
+		  },
+		};
+		
+		let flist = g.sync(rootdir + "/**/" + cleanup4glob(fname), glob_cfg);
+		//console.log({flist});
+		if (flist.length >= 1) {
+			let new_fpath = flist[0];
+			//console.log({new_fpath});
+			
+			edited = true;
+			
+			return `src: url('${new_fpath}')${p2};`.replace(/\\/g, '/');
+		}
+		else {
+			fname = path.basename(fp, path.extname(fname));
+			console.log({rootdir, fname});
+			
+			flist = g.sync(rootdir + "/**/" + cleanup4glob(fname + ".otf"), glob_cfg);
+			console.log({flist});
+			if (flist.length >= 1) {
+				let new_fpath = flist[0];
+				console.log({new_fpath});
+				
+				edited = true;
+				
+				return `src: url('${new_fpath}') format('opentype${variable_suffix}');`.replace(/\\/g, '/');
+			}
+			
+			flist = g.sync(rootdir + "/**/" + cleanup4glob(fname + ".ttf"), glob_cfg);
+			console.log({flist});
+			if (flist.length >= 1) {
+				let new_fpath = flist[0];
+				console.log({new_fpath});
+				
+				edited = true;
+				
+				return `src: url('${new_fpath}') format('truetype${variable_suffix}');`.replace(/\\/g, '/');
+			}
+			
+			//edited = true;
+			
+			//return `src: BAD FONT PATH SPEC! FILE IS LOST!`;
+		}
+	}
+	
+	return m;
+});
+
+if (edited) {
+	fs.writeFileSync(scss_filepath, src, "utf8");
+	console.log("Updated SCSS file:", scss_filepath);
+}
+
+
+
